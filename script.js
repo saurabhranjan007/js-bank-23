853db326aeea9a82b2615eea11ca2c6ce4202d42
Saurabh Ranjan <74584408+saurabhranjan007@users.noreply.github.com>
2023-05-19 22:25:31 +0530

    added new deposit/withdrawal details

--- script.js
+++ script.js
@@ -61,6 +61,36 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// => display more account movements (IN/OUT)
+const displayMovements = function(movements) {
+
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function(mov, i) {
+
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html) // will add the html literal to DOM 
+  })
+}
+
+displayMovements(account1.movements)
+
+// username of the users 
+const user = "Jonas Schmedtmann"; 
+const username = user.toLowerCase().split(' ').map((name) => {
+  return name[0]
+}).join('')
+
+console.log(`username: ${username}`);
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
